aadf38c9a156ead2129811b20e4046d21e994f5d
jtran632 <jimmytran357@yahoo.com>
2022-12-18 08:59:16 -0800

    initial commit should've done sooner almost halfway done

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  images: {
+    loader: "default",
+    domains: ["veekun.com", "projectpokemon.org"],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
